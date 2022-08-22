3da645d6e000f02559a29e371df16989a68282f6
Bret Squire <bretsquire@gmail.com>
2022-08-22 01:26:18 -0400

    Add app name, description and list of features in the onboarding view.

--- GrubSafe/OnboardingView.swift
+++ GrubSafe/OnboardingView.swift
@@ -16,12 +16,37 @@ struct OnboardingView: View {
                 .opacity(0.15)
                 .frame(width: 35, height: 5)
                 .padding()
+            Text("GrubSafe")
+                .font(.largeTitle)
+            Text("A minimal contact restaurant app for safe and healthy dining.")
+                .font(.title3)
+                .padding()
+            VStack(alignment: .leading) {
+                FeatureRow(text: "View the full menu.")
+                FeatureRow(text: "Select and order your meals.")
+                FeatureRow(text: "Add photos of your meals.")
+                FeatureRow(text: "Write reviews of your meals.")
+            }
             Spacer()
             Button("Press to dismiss") {
                 dismiss()
             }
-            Spacer()
+            
         }
+    }
+}
+
+struct FeatureRow: View {
+    let text: String
+    var body: some View {
+        HStack {
+            Image(systemName: "checkmark")
+                .font(.body)
+                .padding()
+            Text(text)
+                .font(.body)
+        }
+        .padding()
     }
 }
 
