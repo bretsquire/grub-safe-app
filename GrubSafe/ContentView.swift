64ce90637063b1c70d1ed3856374869af915acdc
Bret Squire <bretsquire@gmail.com>
2022-08-21 23:25:27 -0400

    Initial Commit

---
+++ GrubSafe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GrubSafe
+//
+//  Created by Bret Squire on 8/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GrubSafe/GrubSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrubSafeApp.swift
+//  GrubSafe
+//
+//  Created by Bret Squire on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct GrubSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
