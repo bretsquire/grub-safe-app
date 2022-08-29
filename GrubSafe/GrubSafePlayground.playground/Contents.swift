7029e28e44f2bfc57d9b24b3636a5af58c1886a3
Bret Squire <bretsquire@gmail.com>
2022-08-29 04:22:53 -0400

    Assignment 1

--- GrubSafe/GrubSafePlayground.playground/Contents.swift
+++ GrubSafe/GrubSafePlayground.playground/Contents.swift
@@ -1,3 +1,16 @@
-import UIKit
+//: # Week 2 Homework Assignment - Minimal contact restaurant App
 
-var greeting = "Hello, playground"
+//: ## Assignmet 1:
+struct MenuItem {
+    let id: String
+    let name: String
+    let description: String?
+    let cost: Double
+    let allergens: String?
+}
+let hotdog = MenuItem(id: "unique-GUID-123",
+                      name: "Beef Hot Dog",
+                      description: "All beef hot dog on a bun with mustard and ketchup.",
+                      cost: 1.50,
+                      allergens: "Contains: gluten")
+print(hotdog)
