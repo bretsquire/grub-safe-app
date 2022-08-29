35b6c82ea8878d8b0a7d97ef7590d04f5e3a4247
Bret Squire <bretsquire@gmail.com>
2022-08-29 04:38:19 -0400

    Assignment 3: make 'description' and 'allergen' properties optional

--- GrubSafe/GrubSafePlayground.playground/Contents.swift
+++ GrubSafe/GrubSafePlayground.playground/Contents.swift
@@ -13,9 +13,27 @@ let hotdog = MenuItemA(id: "unique-GUID-123",
                       description: "All beef hot dog on a bun with mustard and ketchup.",
                       cost: 1.50,
                       allergens: "Contains: gluten")
+print("****** Assignment 1 ******")
 print(hotdog)
 
 //: ## Assignmet 2:
 var menuItems: [MenuItemA] = []
 menuItems.append(hotdog)
+print("\n****** Assignment 2 ******")
 print(menuItems)
+
+//: ## Assignmet 3:
+struct MenuItem {
+    let id: String
+    let name: String
+    let description: String?
+    let cost: Double
+    let allergens: String?
+}
+let porkHotDog = MenuItem(id: "unique-GUID-1234",
+                          name: "Pork Hot Dog",
+                          description: "Pork hot dog on a gluten-free bun with mustard and ketchup.",
+                          cost: 1.50,
+                          allergens: nil)
+print("\n****** Assignment 3 ******")
+print(porkHotDog)
