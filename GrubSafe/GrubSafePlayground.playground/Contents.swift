127d3349b3ec04393f68f1e6ddd92fff90f27638
Bret Squire <bretsquire@gmail.com>
2022-08-29 04:29:49 -0400

    Assignment 2: create array to store menu item struct
    - remove optionals from assignment 1 section for consistency

--- GrubSafe/GrubSafePlayground.playground/Contents.swift
+++ GrubSafe/GrubSafePlayground.playground/Contents.swift
@@ -1,16 +1,21 @@
 //: # Week 2 Homework Assignment - Minimal contact restaurant App
 
 //: ## Assignmet 1:
-struct MenuItem {
+struct MenuItemA {
     let id: String
     let name: String
-    let description: String?
+    let description: String
     let cost: Double
-    let allergens: String?
+    let allergens: String
 }
-let hotdog = MenuItem(id: "unique-GUID-123",
+let hotdog = MenuItemA(id: "unique-GUID-123",
                       name: "Beef Hot Dog",
                       description: "All beef hot dog on a bun with mustard and ketchup.",
                       cost: 1.50,
                       allergens: "Contains: gluten")
 print(hotdog)
+
+//: ## Assignmet 2:
+var menuItems: [MenuItemA] = []
+menuItems.append(hotdog)
+print(menuItems)
