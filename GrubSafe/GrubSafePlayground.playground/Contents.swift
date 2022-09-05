720267588459f24fc18110b6a6d83004876c7d4e
Bret Squire <bretsquire@gmail.com>
2022-09-05 04:52:49 -0400

    Assignment 1:  create totalAmountAfterDiscount function that takes in totalAmount and discountPercentage

--- GrubSafe/GrubSafePlayground.playground/Contents.swift
+++ GrubSafe/GrubSafePlayground.playground/Contents.swift
@@ -1,3 +1,40 @@
+//: # Week 3 Homework Assignment - Minimal contact restaurant App
+/*:
+ Properties/ Data/ Formulae:
+ * itemPrices: An array containing prices of 7 (or any number of) items.
+ * totalAmount: Total amount of all items before applying discount. (A single value.)
+ * discountPercentage: A percentage discount number. For example: 5%, 10%.
+ * discountType: Type of discount to be applied on an item. Discount types could be Default discount (5%), Thanksgiving discount (10%), Christmas discount (15%), New year discount (20%).
+ * discountedAmount: Amount after applying discount on totalAmount. Formula: totalAmount * 0.05 (if a discount of 5% is applied).
+ * totalAmountAfterDiscount: total Amount - discountedAmount.
+ */
+//: ## Assignmet 1: Function
+func totalAfterDiscount(totalAmount: Double, discountPercentage: Double) -> Double {
+    let discountedAmount = totalAmount * discountPercentage
+    return totalAmount - discountedAmount
+}
+
+var totalAmountAfterDiscount = totalAfterDiscount(totalAmount: 1.99, discountPercentage: 0.1)
+print("Assignment 1:\n \(totalAmountAfterDiscount) = 1.99 - (1.99 * 0.1)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //: # Week 2 Homework Assignment - Minimal contact restaurant App
 
 //: ## Assignmet 1:
