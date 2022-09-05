ba8ccf02abd2f103697a9fe46f8f6396b837f43e
Bret Squire <bretsquire@gmail.com>
2022-09-05 10:11:40 -0400

    Assignment 13: applied solution to the app code
    - decided to make the order.total a computed property instead of a stored property calculated by the observer on order.selection

---
+++ GrubSafe/Views/OrderView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderView.swift
+//  GrubSafe
+//
+//  Created by Bret Squire on 9/5/22.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        Text(order.description)
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderView()
+    }
+}
